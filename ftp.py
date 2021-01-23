81ab52b2087ff1ed700458b6c1f2dbe16a5d665c
Joshua Pereyda <joshua.t.pereyda@gmail.com>
2021-01-23 12:34:49 -0800

    Add callback check using new ProtocolSession metadata
    And add --feature-check

--- ftp.py
+++ ftp.py
@@ -1,10 +1,73 @@
 #!/usr/bin/env python
 # Designed for use with boofuzz v0.0.1-dev3
+import re
+
 from boofuzz import *
 from boofuzz.constants import DEFAULT_PROCMON_PORT
 from boofuzz.utils.debugger_thread_simple import DebuggerThreadSimple
 from boofuzz.utils.process_monitor_local import ProcessMonitorLocal
 import click
+
+
+class BooFtpException(Exception):
+    pass
+
+
+def check_reply_code(target, fuzz_data_logger, session, test_case_context, *args, **kwargs):
+    """
+        Args:
+            target (Target): Target with sock-like interface.
+            fuzz_data_logger (ifuzz_logger.IFuzzLogger): Allows logging of test checks and passes/failures.
+                Provided with a test case and test step already opened.
+            session (Session): Session object calling post_send.
+                Useful properties include last_send and last_recv.
+            test_case_context (ProtocolSession): Context for test case-scoped data.
+                :py:class:`TestCaseContext` :py:attr:`session_variables <TestCaseContext.session_variables>`
+                values are generally set within a callback and referenced in elements via default values of type
+                :py:class:`ReferenceValueTestCaseSession`.
+            args: Implementations should include \\*args and \\**kwargs for forward-compatibility.
+            kwargs: Implementations should include \\*args and \\**kwargs for forward-compatibility.
+    """
+    if test_case_context.previous_message.name == "__ROOT_NODE__":
+        return
+    else:
+        try:
+            fuzz_data_logger.log_info("Parsing reply contents: {0}".format(session.last_recv))
+            parse_ftp_reply(session.last_recv)
+        except BooFtpException as e:
+            fuzz_data_logger.log_fail(str(e))
+        fuzz_data_logger.log_pass()
+
+
+def parse_ftp_reply(data):
+    """
+    Parse FTP reply and return reply code. Raise BooFtpException if reply is invalid.
+
+    Note:
+    1. Multi-line replies not supported yet
+    
+    RFC 959 excerpt:
+          A reply is defined to contain the 3-digit code, followed by Space
+          <SP>, followed by one line of text (where some maximum line length
+          has been specified), and terminated by the Telnet end-of-line
+          code.  There will be cases however, where the text is longer than
+          a single line...
+
+    Args:
+        data (bytes): Raw reply data
+    """
+    reply_code_len = 3
+    if len(data) < reply_code_len:
+        raise BooFtpException("Invalid FTP reply, too short; must be a 3-digit sequence followed by a space")
+    else:
+        try:
+            reply = data[0:reply_code_len+1].decode('ascii')
+        except ValueError:
+            raise BooFtpException("Invalid FTP reply, non-ASCII characters; must be a 3-digit sequence followed by a space")
+        if not re.match('[1-5][0-9][0-9] ', reply[0:4]):
+            raise BooFtpException("Invalid FTP reply; must be a 3-digit sequence followed by a space")
+        else:
+            return reply[0:reply_code_len]
 
 
 @click.group()
@@ -24,12 +87,14 @@ def cli():
 @click.option('--procmon-host', help='Process monitor port host or IP')
 @click.option('--procmon-port', type=int, default=DEFAULT_PROCMON_PORT, help='Process monitor port')
 @click.option('--procmon-start', help='Process monitor start command')
+@click.option('--procmon-capture', is_flag=True, help='Capture stdout/stderr from target process upon failure')
 @click.option('--tui/--no-tui', help='Enable/disable TUI')
 @click.option('--text-dump/--no-text-dump', help='Enable/disable full text dump of logs', default=False)
+@click.option('--feature-check', is_flag=True, help='Run a feature check instead of a fuzz test', default=False)
 @click.argument('target_cmdline', nargs=-1, type=click.UNPROCESSED)
-def fuzz(target_cmdline, target_host, target_port, username, password, test_case_index, test_case_name, csv_out,
-         sleep_between_cases,
-         procmon_host, procmon_port, procmon_start, tui, text_dump):
+def fuzz(target_cmdline, target_host, target_port, username, password,
+         test_case_index, test_case_name, csv_out, sleep_between_cases,
+         procmon_host, procmon_port, procmon_start, procmon_capture, tui, text_dump, feature_check):
     local_procmon = None
     if len(target_cmdline) > 0 and procmon_host is None:
         local_procmon = ProcessMonitorLocal(crash_filename="boofuzz-crash-bin",
@@ -52,6 +117,8 @@ def fuzz(target_cmdline, target_host, target_port, username, password, test_case
         procmon_options['start_commands'] = [procmon_start]
     if target_cmdline is not None:
         procmon_options['start_commands'] = [list(target_cmdline)]
+    if procmon_capture:
+        procmon_options['capture_output'] = True
 
     if local_procmon is not None or procmon_host is not None:
         if procmon_host is not None:
@@ -64,21 +131,43 @@ def fuzz(target_cmdline, target_host, target_port, username, password, test_case
         procmon = None
         monitors = []
 
+    start = None
+    end = None
+    fuzz_only_one_case = None
+    if test_case_index is None:
+        start = 1
+    elif "-" in test_case_index:
+        start, end = test_case_index.split("-")
+        if not start:
+            start = 1
+        else:
+            start = int(start)
+        if not end:
+            end = None
+        else:
+            end = int(end)
+    else:
+        fuzz_only_one_case = int(test_case_index)
+
+    connection = TCPSocketConnection(target_host, target_port)
+
     session = Session(
         target=Target(
-            connection=TCPSocketConnection(target_host, target_port),
+            connection=connection,
             monitors=monitors,
-            # procmon=procmon,
-            # procmon_options=procmon_options,
         ),
         fuzz_loggers=fuzz_loggers,
         sleep_time=sleep_between_cases,
+        index_start=start,
+        index_end=end,
     )
 
     initialize_ftp(session, username, password)
 
-    if test_case_index is not None:
-        session.fuzz_single_case(mutant_index=test_case_index)
+    if feature_check:
+        session.feature_check()
+    elif fuzz_only_one_case is not None:
+        session.fuzz_single_case(mutant_index=fuzz_only_one_case)
     elif test_case_name is not None:
         session.fuzz_by_name(test_case_name)
     else:
@@ -113,15 +202,15 @@ def initialize_ftp(session, username, password):
     mkd = _ftp_cmd_1_arg(cmd_code="MKD", default_value="AAAA")
     abor = _ftp_cmd_0_arg(cmd_code="ABOR")
 
-    session.connect(user)
-    session.connect(user, password)
-    session.connect(password, stor)
-    session.connect(password, retr)
-    session.connect(password, mkd)
-    session.connect(password, abor)
-    session.connect(stor, abor)
-    session.connect(retr, abor)
-    session.connect(mkd, abor)
+    session.connect(user, callback=check_reply_code)
+    session.connect(user, password, callback=check_reply_code)
+    session.connect(password, stor, callback=check_reply_code)
+    session.connect(password, retr, callback=check_reply_code)
+    session.connect(password, mkd, callback=check_reply_code)
+    session.connect(password, abor, callback=check_reply_code)
+    session.connect(stor, abor, callback=check_reply_code)
+    session.connect(retr, abor, callback=check_reply_code)
+    session.connect(mkd, abor, callback=check_reply_code)
 
 
 def _ftp_cmd_0_arg(cmd_code):
